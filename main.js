0e4ec2b91a7fd3470123a4cb01d001ec070fa7f8
dawid <dawtom97@gmail.com>
2023-09-23 13:52:01 +0200

    start

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-console.log("test");
 
-console.log("kolejny log");
-
-console.log("jeszcze jeden");
-
-console.log("dawid zmiana");
-
-console.log("siema");
-
-console.log("DAWID ZMIANA 2");
-
-console.log("Byłem tu");
