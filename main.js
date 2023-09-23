49a4269cc6630fafac2163d947e8fe1054d3ec9d
dawid <dawtom97@gmail.com>
2023-09-23 14:02:25 +0200

    database connection

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const MONGO_URL = "mongodb+srv://dawid:dawid123@cluster0.j8lhl.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("Błąd połączenia z bazą danych", err);
+});
+
+db.once("open", () => {
+    console.log("Połączono z bazą danych")
+});
