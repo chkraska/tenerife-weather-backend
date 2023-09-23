2a98fb7d54fa3605f39df0de151b59150f8e178e
chkraska <kraska.chrystian@gmail.com>
2023-09-23 13:47:43 +0200

    zmianka

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-console.log("test")
+console.log("test");
 
-console.log("kolejny log")
+console.log("kolejny log");
 
+console.log("jeszcze jeden");
 
-console.log("jeszcze jeden")
+console.log("dawid zmiana");
 
-console.log("dawid zmiana")
+console.log("siema");
 
-console.log("siema")
+console.log("DAWID ZMIANA 2");
 
-
-console.log("DAWID ZMIANA 2")
-
+console.log("Byłem tu");
